585c0c3c7efe13e65012426511986302bd399fc4
aurore <aurore@aurore.com>
2016-03-07 12:03:21 +0100

    pelicanconf reverted

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,7 +22,7 @@ STATIC_PATHS = ['static']
 
 THEME = 'theme'
 #THEME = 'simple'
-DIRECT_TEMPLATES = ['blog','categories','archives']
+DIRECT_TEMPLATES = ['blog']
 PAGINATED_DIRECT_TEMPLATES = ['blog']
 
 # Feed generation is usually not desired when developing
